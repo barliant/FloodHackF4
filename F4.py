bead29b60e1cbc173267c27ae43b913a8f101319
Anung Ariwibowo <barliant@gmail.com>
2016-04-07 22:05:51 +0700

    Initial try using Kivy.

---
+++ F4.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class FloodApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    FloodApp().run()
+
+
---
+++ main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+
+
+class FloodApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    FloodApp().run()
+
